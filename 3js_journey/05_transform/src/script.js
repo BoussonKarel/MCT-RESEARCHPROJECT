402a3baa3fd41255dd425f5ae0378ddac5daf91a
Karel <karel.bousson@student.howest.be>
2022-01-10 13:27:26 +0100

    transform

--- 3js_journey/05_transform/src/script.js
+++ 3js_journey/05_transform/src/script.js
@@ -13,6 +13,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+// Add to scene
 scene.add(mesh)
 
 /**
@@ -29,6 +30,58 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// AXES HELPER
+const axesHelper = new THREE.AxesHelper(1); // AxesHelper(length)
+scene.add(axesHelper);
+
+// POSITION
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+
+// POSITION - METHODS
+mesh.position.set(0.7, -0.6, 1)
+
+console.log(
+    "Length between cube & center ",
+    mesh.position.length())
+console.log(
+    "Length between cube & center ",
+    mesh.position.distanceTo(camera.position))
+
+// mesh.position.normalize() // Reduce length() until it's 1
+
+// SCALE
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5)
+
+// ROTATION VS QUATERNION
+// Updating one updates the other :)
+
+// ROTATION
+// rotation is not Vector3, but Euler
+
+ // First rotate around Y, then X, then Z
+// Otherwise you will get gimball lock
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI*0.25; // 1 full rotation = PI
+mesh.rotation.y = Math.PI*0.25; // 1 full rotation = PI
+
+// QUATERNION
+// Rotation, but in a more mathematical way
+
+// LOOKAT
+// Make camera look at the center of an object
+// lookAt(Vector3)
+camera.lookAt(mesh.position)
+
+// COMBINING TRANSFORMATIONS
+// You can combine transformations in any order (scale, position, rotation)
+
+
 
 /**
  * Renderer
