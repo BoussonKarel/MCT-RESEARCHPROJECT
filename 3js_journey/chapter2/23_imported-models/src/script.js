77ec1462b246b363f5240d726a44493e5a327198
Karel <karel.bousson@student.howest.be>
2022-01-18 18:28:07 +0100

    23 done

--- 3js_journey/chapter2/23_imported-models/src/script.js
+++ 3js_journey/chapter2/23_imported-models/src/script.js
@@ -1,7 +1,9 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+import "./style.css"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import * as dat from "lil-gui"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
 
 /**
  * Base
@@ -10,24 +12,95 @@ import * as dat from 'lil-gui'
 const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl")
 
 // Scene
 const scene = new THREE.Scene()
 
 /**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/') // use workers
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+// gltfLoader.load(
+//   // "/models/Duck/glTF/Duck.gltf",
+//   "/models/Duck/glTF-Binary/Duck.glb",
+//   (gltf) => {
+//     scene.add(gltf.scene.children[0])
+//     // The Duck mesh is located at scene.children[0].children[1]
+//   }
+// )
+
+// gltfLoader.load(
+//   "/models/FlightHelmet/glTF/FlightHelmet.gltf",
+//   (gltf) => {
+//     console.log(gltf)
+//     // The Duck mesh is located at scene.children[0].children[1]
+//     // for (const child of gltf.scene.children) {
+//     //   scene.add(child)
+//     // }
+//     // This 'for' loop doesn't work, because it removes it from gltf.scene
+//     // Everything moves in the children array
+
+//     // Alternative: while
+//     // while (gltf.scene.children.length) {
+//     //   scene.add(gltf.scene.children[0])
+//     // }
+
+//     // OR duplicate the array & then loop
+//     const children = [...gltf.scene.children]
+//     for (const child of children) {
+//       scene.add(child)
+//     }
+
+//     // OR scene.add(gltf.scene)
+//   }
+// )
+
+// DRACO COMPRESSION
+// gltfLoader.load(
+//   "/models/Duck/glTF-Draco/Duck.gltf",
+//   (gltf) => {
+//     scene.add(gltf.scene.children[0])
+//   }
+// )
+
+let mixer = null
+
+// ANIMATIONS
+gltfLoader.load(
+  "/models/Fox/glTF/Fox.gltf",
+  (gltf) => {
+    console.log(gltf)
+    // No animation when doing sceneadd(gltf.scene) :'(
+
+    mixer = new THREE.AnimationMixer(gltf.scene)
+    const action = mixer.clipAction(gltf.animations[1])
+    
+    action.play()
+
+    gltf.scene.scale.set(0.025, 0.025, 0.025)
+    scene.add(gltf.scene)
+  }
+)
+
+/**
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  })
 )
 floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
+floor.rotation.x = -Math.PI * 0.5
 scene.add(floor)
 
 /**
@@ -40,10 +113,10 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.left = -7
 directionalLight.shadow.camera.top = 7
 directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
+directionalLight.shadow.camera.bottom = -7
 directionalLight.position.set(5, 5, 5)
 scene.add(directionalLight)
 
@@ -51,30 +124,34 @@ scene.add(directionalLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 camera.position.set(2, 2, 2)
 scene.add(camera)
 
@@ -87,7 +164,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
@@ -100,20 +177,22 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let previousTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
-    // Update controls
-    controls.update()
+  // Update mixer
+  if (mixer) mixer.update(deltaTime)
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
